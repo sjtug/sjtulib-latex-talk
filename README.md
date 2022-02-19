071e3e9b8c1f672875011a5a6bda5f0970c59715
LogCreative <lizilong123456@live.cn>
2022-02-19 22:15:24 +0800

    setup l3build for typesetting

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# 如何使用 LaTeX 排版论文
+# 从零开始使用 LaTeX 排版论文
 
-上海交通大学图书馆专题培训讲座 《如何使用 LaTeX 排版论文》。
+上海交通大学图书馆专题培训讲座 2022 年《从零开始使用 LaTeX 排版论文》。
 
-以可以使用 XeLaTeX 引擎编译 `latex-talk.tex` 文件。
+第一次编译以及依赖文件更改后请使用
+```
+l3build doc
+```
+编译文件 `latex-talk-2022.tex`。
+
+之后可以使用默认编译方法编译主文件。
 
 可以通过 [GitHub Issues](https://github.com/sjtug/sjtulib-latex-talk/issues) 报告文档中的 BUG 等。
 
--- build.lua
+++ build.lua
@@ -3,25 +3,76 @@ module       = "latex-talk"
 docfiledir   = "."
 typesetfiles = {"latex-talk-2022.tex"}
 
--- TODO: l3build doc will typeset the documentation.
--- TODO: hook the "l3build doc" method to ".vscode/settings.json"
+-- Select the typesetting engine for different platforms. 
+if os.type == "windows" then
+    typesetexe       = "pdflatex"
+else
+    typesetexe       = "xelatex"
+end
 
+supportdir = "support"
+
+-- typseset_demo_tasks() start after support file copying
+-- and before the main document is typeset.
 function typeset_demo_tasks()
     local errorlevel = 0
 
-    if fileexists("beamerthemesjtubeamer.sty") == false then
-        -- Clone the theme first.
-        errorlevel = os.execute("git clone https://mirror.sjtu.edu.cn/git/SJTUBeamer.git/")
-        if (errorlevel ~= 0) then 
-            print("git clone SJTUBeamer failed")
-            return errorlevel
+    -- Refresh dependencies: SJTUBeamer, SJTUThesis
+    local beamerdepdir = "SJTUBeamer"
+    local thesisdepdir = "SJTUThesis"
+    for _, dir in ipairs({beamerdepdir, thesisdepdir}) do
+        if direxists(dir) == false then
+            -- Clone the dependencies first.
+            errorlevel = os.execute("git clone https://mirror.sjtu.edu.cn/git/" .. dir .. ".git/")
+            if (errorlevel ~= 0) then 
+                print("git clone " .. dir .. " failed")
+                return errorlevel
+            end
+        else
+            -- Update the dependencies.
+            run(dir, "git pull")
         end
-        -- TODO: copy the theme to the current directory.
     end
 
-    -- TODO: typeset pdfs and cache them. Or use tikz. TBD.
+    -- Required files in SJTUBeamer.
+    beamerdepreq = {"*.sty","contrib/","vi/"}
+    -- Move the required dependencies to the current folder
+    -- for compatibility with overleaf version.
+    -- and to the doc build directory for l3build typesetting.
+    for _, file in pairs(beamerdepreq) do
+        cp(file, beamerdepdir, docfiledir)
+        cp(file, beamerdepdir, typesetdir)
+    end
 
-    -- TODO: accelerate the compilation throught etex.
+    -- Compile the thesis on latexmk,
+    -- since it is a custom compiling environment.
+    local typesetthesisexe = "latexmk"
+    -- Remember, although previous "git pull" 
+    -- refreshed the tex file to default state,
+    -- here we move the unchanged support files 
+    -- to the dependency directory again 
+    -- to overwrite the default files.
+    local suppthesisdir = supportdir .. "/thesis"
+    cp("*", suppthesisdir, thesisdepdir)
+    local typesetthesisfiles = {"main.tex"} -- To be added "dev-v2.tex"
+    for _, file in ipairs(typesetthesisfiles) do
+        errorlevel = os.execute("cd " .. thesisdepdir .. " && " .. typesetthesisexe .. " " .. file)
+        local pdffile = file:gsub("%.tex$", ".pdf")
+        cp(pdffile, thesisdepdir, suppthesisdir)
+        if (errorlevel ~= 0) then 
+            print(typesetthesisexe .. " " .. file .. " failed")
+            return errorlevel
+        end
+    end
+
+    -- TODO: typeset auxiliary pdfs and cache them. Or use tikz. TBD.
+
+    -- TODO: accelerate the compilation through etex.
+
+    -- Rerun the support file copying,
+    -- because the typesetsuppfiles is set to none
+    -- in the first place and doesn't trigger the process.
+    cp(supportdir, ".", typesetdir)
 
     return errorlevel
 end
